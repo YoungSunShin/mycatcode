b3f319b42e1e8a5f985c749245c9b482e4a32623
YoungSun Shin <tlsdudtjs128@gmail.com>
2018-11-20 21:15:00 +0900

    #8 Add log in to  activitys

--- app/src/main/java/kr/emirim/youngsunshin/mycatcode/SplashActivity.java
+++ app/src/main/java/kr/emirim/youngsunshin/mycatcode/SplashActivity.java
@@ -2,12 +2,16 @@ package kr.emirim.youngsunshin.mycatcode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
+
+    public static final String TAG = "스플레시액티비티";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Log.d(TAG,"onCreate 실행");
     }
 }
